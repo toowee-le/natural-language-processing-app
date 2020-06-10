d06a8137e90aaddf12e80ad97152b8ab9683f69e
Tuyet Le <toowee.le@gmail.com>
2020-06-10 17:09:49 +0100

    set production and development mode and create new config file accordingly

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,6 +3,7 @@ const webpack = require('webpack');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'production',
     entry: './src/index.js',
     module: {
         rules: [
---
+++ webpack.prod.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const webpack = require('webpack');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './src/client/views/index.html',
+            filename: './index.html',
+        })
+    ]
+}
