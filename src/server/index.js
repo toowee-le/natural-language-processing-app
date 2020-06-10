5d4023c63106f58c775e883ef8d0a93b55fa761d
Tuyet Le <toowee.le@gmail.com>
2020-06-10 16:39:33 +0100

    update express server to change the home route to use the index file from dist

--- src/server/index.js
+++ src/server/index.js
@@ -9,8 +9,8 @@ app.use(express.static('dist'));
 console.log(__dirname);
 
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'));
+    res.sendFile('dist/index.html')
+    // res.sendFile('/client/views/index.html', { root: __dirname + '/..' });
 })
 
 // Designates what port the app will listen to for incoming requests
