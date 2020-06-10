db043abcfe852c86292d3bc898727dc0f3077b6e
Tuyet Le <toowee.le@gmail.com>
2020-06-10 13:59:32 +0100

    add starter code from udacity repo

---
+++ src/server/index.js
@@ -0,0 +1,21 @@
+var path = require('path');
+const express = require('express');
+const mockAPIResponse = require('./mockAPI.js');
+
+const app = express();
+
+app.use(express.static('dist'));
+
+console.log(__dirname);
+
+app.get('/', function (req, res) {
+    // res.sendFile('dist/index.html')
+    res.sendFile(path.resolve('src/client/views/index.html'));
+})
+
+// Designates what port the app will listen to for incoming requests
+app.listen(8080, () => { console.log('App listening on port 8080!') });
+
+app.get('/test', function (req, res) {
+    res.send(mockAPIResponse);
+});
