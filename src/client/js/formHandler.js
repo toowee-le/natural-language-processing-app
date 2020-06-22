7aa099d35cec8d42e2185f49c3f70d8aa6ce7cc7
Tuyet Le <toowee.le@gmail.com>
2020-06-22 21:08:02 +0100

    update babel

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -15,7 +15,7 @@ export function performNLP(event) {
 
     if (checkURL(url)) {
         preloader('show');
-        postRequest('http://localhost:8000/apiCall', { url })
+        postRequest('http://localhost:5000/apiCall', { url })
         .then(data => {
             updateUI(data);
             preloader('hide');
