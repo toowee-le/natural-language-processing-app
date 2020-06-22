18b4346e21fd338dd62a0999f545ade8fc94902d
Tuyet Le <toowee.le@gmail.com>
2020-06-22 20:31:19 +0100

    update button for ios devices

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -111,7 +111,7 @@ define("./service-worker.js",['./workbox-64f1e998'], function (workbox) { 'use s
     "revision": "6e82c28ed670d07f834bcb1e64ac8e45"
   }, {
     "url": "main.css",
-    "revision": "2b9109672577c6c93546c1929da0e397"
+    "revision": "a6623f2b1c7b61495816d1ec176a9bf1"
   }, {
     "url": "main.js",
     "revision": "0dc0e0eae38cc5485cf6ea8bc254cd17"
